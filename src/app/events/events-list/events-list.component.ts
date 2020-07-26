1213f579032efaf1b3424447dd6c4e4812800c22
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-07-25 20:16:26 -0700

    First data-bound component

--- src/app/events/events-list/events-list.component.ts
+++ src/app/events/events-list/events-list.component.ts
@@ -6,6 +6,19 @@ import { Component } from "@angular/core";
 })
 export class EventsListComponent {
 
-    
+    // hardcoded data object
+    event = {
+        id: 1,
+        name: "Angular connect",
+        date: "9/26/2036",
+        time: "10:00 am",
+        price: 599.99,
+        imageUrl: "/assets/images/angularconnect-shield.png",
+        location: {
+            address: "1057 DT",
+            country: "England",
+            city: "London"
+        }
+    };
 
 }
