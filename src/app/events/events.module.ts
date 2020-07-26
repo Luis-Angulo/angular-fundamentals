d9aa4ca56042bafda8a9a74613a11a12e5b05bc7
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-07-25 20:28:47 -0700

    Communicating with child components

--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import {EventsListComponent} from "./events-list/events-list.component";
+import { EventThumbnailComponent } from "./event-thumbnail/event-thumbnail.component";
 
 @NgModule({
     imports: [],
-    declarations: [EventsListComponent],
+    declarations: [EventsListComponent, EventThumbnailComponent],
     exports: [EventsListComponent],
 })
 export class EventsModule {}
