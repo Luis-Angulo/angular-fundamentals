d90eaf5e3d91bf9ea7658905f3d982a588b52586
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-08-07 02:13:28 -0700

    Custom pipes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CollapsibleWellComponent } from './collapsible-well/collapsible-well.component';
+import { DurationPipe } from './duration.pipe';
 
 @NgModule({
-  declarations: [CollapsibleWellComponent],
+  declarations: [CollapsibleWellComponent, DurationPipe],
   imports: [CommonModule],
-  exports: [CollapsibleWellComponent],
+  exports: [CollapsibleWellComponent, DurationPipe],
 })
 export class SharedModule {}
