f6964bc975853cee2f4499468f66fe1b169a1a88
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-08-12 12:59:57 -0700

    Minor test fixes

--- src/app/shared/modal-trigger.directive.spec.ts
+++ src/app/shared/modal-trigger.directive.spec.ts
@@ -2,6 +2,7 @@ import { ModalTriggerDirective } from './modal-trigger.directive';
 
 describe('ModalTriggerDirective', () => {
   it('should create an instance', () => {
+    // Check how to fix the ref issue later
     const directive = new ModalTriggerDirective();
     expect(directive).toBeTruthy();
   });
