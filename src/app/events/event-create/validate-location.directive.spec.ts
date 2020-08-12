7a9e274753e0d2263fb6971a45bfdf698c1a9e2c
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-08-12 14:12:15 -0700

    FIrst unit test

--- src/app/events/event-create/validate-location.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ValidateLocationDirective } from './validate-location.directive';
-
-describe('ValidateLocationDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ValidateLocationDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/events/vote.service.spec.ts
+++ src/app/events/vote.service.spec.ts
@@ -1,16 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 
 import { VoteService } from './vote.service';
+import { Session } from '../types/session.type';
+import { of } from 'rxjs';
 
 describe('VoteService', () => {
   let service: VoteService;
+  let mockHttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(VoteService);
+    // voteService requires an httpClient, so we pass in a Jasmine spy
+    mockHttpClient = jasmine.createSpyObj('mockHttp', ['delete', 'post']);
+    // angularCLI generated tests use the angular test bed, but this is not covered
+    // in the course, instead we init the service to be tested manually
+    service = new VoteService(mockHttpClient);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('deleteVoter', () => {
+    it('should remove the voter from the list', () => {
+      // arrange
+      const session = { id: 1, voters: ['joe', 'jack'] }; // dummy list
+      mockHttpClient.delete.and.returnValue(of(false));
+
+      // act
+      service.deleteVoter(3, session as Session, 'joe');
+
+      // assert
+      expect(session.voters.length).toBe(1);
+      expect(session.voters[0]).toBe('jack');
+    });
   });
 });
