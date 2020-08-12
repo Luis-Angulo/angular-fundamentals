99adb8d25ac4437a92c9f6f6c3dfe7553e7aabd7
Luis Francisco Angulo Andrade <luis.angulo@sonorasoft.com>
2020-08-12 11:35:05 -0700

    Login to test backend

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,23 +1,31 @@
 import { Injectable } from '@angular/core';
 import { User } from '../types/user.type';
+import { HttpClient } from '@angular/common/http';
+import { tap, catchError } from 'rxjs/operators';
+import { of, Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  private baseUrl = '/api';
   public currentUser: User;
+
+  constructor(private httpClient: HttpClient) {}
 
   updateUserProfile(firstName: string, lastName: string): void {
     this.currentUser.firstName = firstName;
     this.currentUser.lastName = lastName;
   }
 
-  public login(userName: string, password: string): void {
-    this.currentUser = {
-      id: 1,
-      firstName: 'John',
-      lastName: 'Pappa',
-      userName,
-      password,
-    };
+  public login(userName: string, password: string) {
+    return this.httpClient
+      .post(`${this.baseUrl}/login`, { username: userName, password })
+      .pipe(
+        tap((response: any) => {
+          console.log(response);
+          this.currentUser = response.user;
+        })
+      )
+      .pipe(catchError(() => of(false)));
   }
 
   public isAuthenticated(): boolean {
--- src/app/user/user-login/user-login.component.ts
+++ src/app/user/user-login/user-login.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { AuthService } from 'src/app/shared/auth.service';
 import { Router } from '@angular/router';
+import { TOASTR_TOKEN, Toastr } from 'src/app/shared/toastr.service';
 
 @Component({
   selector: 'app-user-login',
@@ -10,16 +11,36 @@ import { Router } from '@angular/router';
 export class UserLoginComponent implements OnInit {
   public userName: string;
   public password: string;
+  public isLoginInvalid: boolean;
+
   // template sets this property to show validation errors if user hovers the login button
   public mouseOverLogin: boolean;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(
+    private authService: AuthService,
+    @Inject(TOASTR_TOKEN) private toastr: Toastr,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.isLoginInvalid = false;
+  }
 
   login(formValues: any): void {
-    this.authService.login(formValues.userName, formValues.password);
-    this.router.navigate(['events']);
+    // the dev server provided doesn't actually check if the password is valid
+    // it only checks that the username exists on records
+    this.authService
+      .login(formValues.userName, formValues.password)
+      .subscribe((res) => {
+        if (res) {
+          this.isLoginInvalid = false;
+          this.toastr.success(`${this.userName} logged in!`);
+          this.router.navigate(['events']);
+        } else {
+          this.isLoginInvalid = true;
+          this.toastr.error(`User: ${this.userName} not found!`);
+        }
+      });
   }
 
   cancel(): void {
